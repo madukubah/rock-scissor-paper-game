e08fa91523826b939e423faec0a6a58039a29cd8
nfaaddillaaah <120910671+nfaaddillaaah@users.noreply.github.com>
2023-03-26 13:35:09 +0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
-print('hi ini alan')
+import random
+import time
+bot=['batu','guting','kertas']
+
+print("hallo world".lower())
+while True:
+    x = random.choice(bot)
+    data = input('Masukkan pilihan')
+    if data.lower() == x:

908883353d87e6a1a6ce769ca870834c1f5481fa
madukubah <muhalfalah1998@gmail.com>
2023-03-26 14:35:44 +0800

    input

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
 print('hi ini alan-branch yuhuu')
+
+print('selamat datang di game btau gunting kertas')
+
+player = 0
+player2 = 0
+
+player = int(input("player 1 : "))
+player2 = int(input("player 2 : "))
